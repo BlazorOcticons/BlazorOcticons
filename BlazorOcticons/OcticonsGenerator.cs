396485e60accf87629ab53c8d57ccbad38e305d6
Evgeniy Kosjakov <42_@mail.ru>
2021-10-31 18:30:27 +0300

    initial

---
+++ BlazorOcticons/OcticonsGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace BlazorOcticons
+{
+    [Generator]
+    public class OcticonsGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context) { }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var source = @"
+namespace BlazorOcticons {{
+    public class Test {{
+        public string Title {{ get; set; }}
+    }}
+}}";
+            context.AddSource("Test.cs", SourceText.From(source, Encoding.UTF8));
+        }
+    }
+}
